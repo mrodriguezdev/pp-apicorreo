4a2bbf5a25af26095300633825308baeb5aa586c
mrodriguezdev <marioguzman_99@hotmail.com>
2024-01-02 21:53:21 -0600

    Creando interfaz para obtener las configuraciones generales de la api. Configurando el archivo application.properties. Creando la dto de respuesta para el envió del email

--- src/main/java/mrodriguezdev/me/apicorreo/application/services/EmailUserCase.java
+++ src/main/java/mrodriguezdev/me/apicorreo/application/services/EmailUserCase.java
@@ -2,18 +2,36 @@ package mrodriguezdev.me.apicorreo.application.services;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import mrodriguezdev.me.apicorreo.domains.configuration.Apicorreo;
 import mrodriguezdev.me.apicorreo.domains.models.EmailDTO;
+import mrodriguezdev.me.apicorreo.domains.models.EmailResponse;
 import mrodriguezdev.me.apicorreo.domains.ports.in.EmailInputPort;
 import mrodriguezdev.me.apicorreo.domains.ports.out.EmailOutputPort;
 
+import java.time.LocalDate;
+
 @ApplicationScoped
 public class EmailUserCase implements EmailInputPort {
+
+    @Inject
+    Apicorreo apicorreo;
 
     @Inject
     EmailOutputPort emailOutputPort;
 
     @Override
     public void sendMail(EmailDTO emailDTO) {
-        this.emailOutputPort.sendMail(emailDTO);
+        EmailResponse emailResponse = this.generateEmailResponse(emailDTO);
+        this.emailOutputPort.sendMail(emailResponse);
+    }
+
+    private EmailResponse generateEmailResponse(EmailDTO emailDTO) {
+        EmailResponse emailResponse = new EmailResponse();
+        emailResponse.setNombre(emailDTO.getNombre());
+        emailResponse.setCorreo(emailDTO.getCorreo());
+        emailResponse.setAsunto(emailDTO.getAsunto());
+        emailResponse.setDomain(this.apicorreo.webAppDomain());
+        emailResponse.setCurrentyear(LocalDate.now().getYear());
+        return emailResponse;
     }
 }
--- src/main/java/mrodriguezdev/me/apicorreo/infraestructure/adapters/EmailAdapter.java
+++ src/main/java/mrodriguezdev/me/apicorreo/infraestructure/adapters/EmailAdapter.java
@@ -1,14 +1,14 @@
 package mrodriguezdev.me.apicorreo.infraestructure.adapters;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import mrodriguezdev.me.apicorreo.domains.models.EmailDTO;
+import mrodriguezdev.me.apicorreo.domains.models.EmailResponse;
 import mrodriguezdev.me.apicorreo.domains.ports.out.EmailOutputPort;
 
 @ApplicationScoped
 public class EmailAdapter implements EmailOutputPort {
 
     @Override
-    public void sendMail(EmailDTO emailDTO) {
-        System.out.println("Información a enviar: " + emailDTO);
+    public void sendMail(EmailResponse emailResponse) {
+        System.out.println("Información a enviar: " + emailResponse);
     }
 }
